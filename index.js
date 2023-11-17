23a86f9d3dd9331a4e4ffdb2dc6d368748298f82
BobTheBu1ld3r <123880897+BobTheBu1ld3r@users.noreply.github.com>
2023-11-17 16:00:50 +0000

    Implement driver script

--- index.js
+++ index.js
@@ -1,1 +1,26 @@
+import Tree, { prettyPrint } from "./bst.js";
 
+const tree = new Tree([1, 7, 4, 23, 8, 9, 4, 3, 5, 7, 9, 67]);
+
+console.log(`Is the tree balanced? : ${tree.isBalanced()}`);
+
+console.log(`Level Order : ${tree.levelOrder()}`);
+console.log(`Pre Order : ${tree.preOrder()}`);
+console.log(`In Order : ${tree.inOrder()}`);
+console.log(`Post Order : ${tree.postOrder()}`);
+
+tree.insert(101);
+tree.insert(102);
+tree.insert(103);
+tree.insert(104);
+
+console.log(`Is the tree balanced? : ${tree.isBalanced()}`);
+
+tree.reBalance();
+
+console.log(`Is the tree balanced? : ${tree.isBalanced()}`);
+
+console.log(`Level Order : ${tree.levelOrder()}`);
+console.log(`Pre Order : ${tree.preOrder()}`);
+console.log(`In Order : ${tree.inOrder()}`);
+console.log(`Post Order : ${tree.postOrder()}`);
